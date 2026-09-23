62391d7cdea26217b04f041fd4f1c31ed68436c6
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2026-09-23 14:56:24 +0300

    documentation

--- src/models/Lesson.js
+++ src/models/Lesson.js
@@ -4,8 +4,11 @@ const sequelize = require('../db');
 const Lesson = sequelize.define('Lesson', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   title: { type: DataTypes.STRING, allowNull: false },
+  slug: { type: DataTypes.STRING, unique: true, allowNull: false },
   content: { type: DataTypes.TEXT, allowNull: false },
-  duration: { type: DataTypes.INTEGER, defaultValue: 15 }
+  duration: { type: DataTypes.INTEGER, allowNull: false },
+  level: { type: DataTypes.STRING, allowNull: false, defaultValue: 'Beginner' },
+  authorId: { type: DataTypes.INTEGER, allowNull: false }
 });
 
 module.exports = Lesson;
